918f85bff05b1c90102aab1ef7d694f3c876cb01
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2023-02-01 18:36:28 +0100

    #E2 Afficher une liste de toutes les transactions dans l’ordre chronologique

--- api.py
+++ api.py
@@ -14,12 +14,18 @@ def hello_world():
 
 #E1 Enregistrer une transaction
 @app.route('/transactions/<transaction>', methods=['POST'])
-def add_trans(transaction):
+def add_transaction(transaction):
         ret = "Transaction"
         if request.method == 'POST':
                 transactions[len(transactions)] = transaction
                 return "You just had " + transaction + " to your transactions !"
 
+
+#E2 Afficher une liste de toutes les transactions dans l’ordre chronologique
+@app.route('/transactions', methods=['GET'])
+def get_transaction():
+        if request.method == 'GET':
+                return str(transactions)
 
 
 if __name__ == '__main__':
